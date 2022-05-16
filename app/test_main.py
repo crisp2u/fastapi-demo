63cb2361d9bf511c48c21c77aea244cbfd4f2e6b
cristian.pop <crisp2u@gmail.com>
2022-05-17 00:19:47 +0300

    test OIDC integration

---
+++ app/test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == "OK"
